874c15ddd13b61553a7d4ea81066458d08b6f00a
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-12-19 15:24:07 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,27 @@
+
+// const {PriorityQueue} = require('@datastructures-js/priority-queue');
+/*
+ PriorityQueue is internally included in the solution file on leetcode.
+ So, when running the code on leetcode it should stay commented out. 
+ It is mentioned here as a comment, just for information about 
+ which external library is applied for this data structure.
+ */
+
+function pickGifts(gifts: number[], targetNumberOfSeconds: number): number {
+    const maxHeapForGift = new PriorityQueue({ compare: (x, y) => y - x });
+    let sumRemainingGiftsAfterTargetTime = 0;
+    for (let gift of gifts) {
+        maxHeapForGift.enqueue(gift);
+        sumRemainingGiftsAfterTargetTime += gift;
+    }
+
+    while (targetNumberOfSeconds > 0) {
+        const gift = maxHeapForGift.dequeue();
+        const remainingGift = Math.floor(Math.sqrt(gift));
+        sumRemainingGiftsAfterTargetTime -= gift - remainingGift;
+        maxHeapForGift.enqueue(remainingGift);
+        --targetNumberOfSeconds;
+    }
+
+    return sumRemainingGiftsAfterTargetTime;
+};
